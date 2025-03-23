c67b3a80d8afb09eb45d5659e0b96f286947a76c
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-03-24 00:00:40 +0900

    * 정상적으로 입력 및 출력이 되는지 테스트를 하기 위해 main문 수정 * 필요없는 인클루드 제거 * 정상적으로 함수 호출이 가능하도록 DLLLoader 수정   * void 타입 대응을 위한 템플릿 특수화 추가   * 정상적으로 함수를 호출하게 하도록 CallFunction 및 AddFunction 수정   * functions의 value를 std::any로 갖게 하고, 캐스팅해서 함수를 호출하도록 수정

--- NonstopPatcher/NonstopPatcher/DLLLoader.h
+++ NonstopPatcher/NonstopPatcher/DLLLoader.h
@@ -10,6 +10,9 @@
 #include <optional>
 #include <any>
 
+template<typename ReturnType, typename... InputType>
+concept VoidReturnType = std::is_void_v<ReturnType>;
+
 class DLLInfo
 {
 public:
@@ -64,18 +67,28 @@ public:
 	}
 
 	template<typename ReturnParamType, typename... InputParamTypes>
-	void AddFunction(const std::string& functionName, std::function<ReturnParamType(InputParamTypes...)> function)
+	bool AddFunction(const std::string& functionName) 
 	{
 		std::unique_lock lock(*mutex);
 
-		functions[functionName] = [function](InputParamTypes... inputParams)
+		auto dllFunc = (ReturnParamType(*)(InputParamTypes&...))GetProcAddress(dllHandle, functionName.c_str());
+		if (dllFunc == nullptr)
 		{
-			function(inputParams...);
+			return false;
+		}
+
+		std::function<ReturnParamType(InputParamTypes&...)> func = [dllFunc](InputParamTypes&... inputParams) -> ReturnParamType
+		{
+			return dllFunc(inputParams...);
 		};
+		functions[functionName] = std::any(func);
+
+		return true;
 	}
 
 	template<typename ReturnParamType, typename... InputParamTypes>
-	std::optional<ReturnParamType> CallFunction(const std::string& functionName, InputParamTypes... inputParams)
+	requires(not VoidReturnType<ReturnParamType>)
+	std::optional<ReturnParamType> CallFunction(const std::string& functionName, InputParamTypes&... inputParams)
 	{
 		std::shared_lock lock(*mutex);
 
@@ -85,15 +98,29 @@ public:
 			return std::nullopt;
 		}
 
-		const std::function<ReturnParamType(InputParamTypes...)>& func = std::any_cast<std::function<ReturnParamType(InputParamTypes...)>>(itor->second);
+		std::function<ReturnParamType(InputParamTypes&...)> func = std::any_cast<std::function<ReturnParamType(InputParamTypes&...)>>(itor->second);
 		return func(inputParams...);
+	}
+
+	template<typename ReturnParamType, typename... InputParamTypes>
+	requires(VoidReturnType<ReturnParamType>)
+	void CallFunction(const std::string& functionName, InputParamTypes&... inputParams)
+	{
+		auto it = functions.find(functionName);
+		if (it == functions.end())
+		{
+			return;
+		}
+
+		std::function<void(InputParamTypes&...)> func = std::any_cast<std::function<void(InputParamTypes&...)>>(it->second);
+		func(inputParams...);
 	}
 
 private:
 	std::string dllPath{};
 	HMODULE dllHandle{};
 	std::shared_ptr<std::shared_mutex> mutex;
-	std::map<std::string, std::function<void()>> functions{};
+	std::map<std::string, std::any> functions{};
 };
 
 class DLLManager
@@ -112,6 +139,51 @@ public:
 	bool LoadDLL(const DLLType dllType, const std::string& dllPath);
 	void LoadDLLAsync(const DLLType dllType, const std::string& dllPath);
 	void UnloadDLL(const DLLType dllType);
+
+public:
+	template<typename ReturnParamType, typename... InputParamTypes>
+	bool AddFunction(const DLLType dllType, const std::string& functionName)
+	{
+		std::shared_lock lock(dllHandlesMutex);
+
+		auto itor = dllHandles.find(dllType);
+		if (itor == dllHandles.end())
+		{
+			return false;
+		}
+
+		return itor->second.AddFunction<ReturnParamType, InputParamTypes&...>(functionName);
+	}
+
+	template<typename ReturnParamType, typename... InputParamTypes>
+	requires(not VoidReturnType<ReturnParamType>)
+	std::optional<ReturnParamType> CallFunction(const DLLType dllType, const std::string& functionName, InputParamTypes&... inputParams)
+	{
+		std::shared_lock lock(dllHandlesMutex);
+
+		auto itor = dllHandles.find(dllType);
+		if (itor == dllHandles.end())
+		{
+			return std::nullopt;
+		}
+
+		return itor->second.CallFunction<ReturnParamType, InputParamTypes&...>(functionName, inputParams...);
+	}
+
+	template<typename ReturnParamType, typename... InputParamTypes>
+	requires(VoidReturnType<ReturnParamType>)
+	void CallFunction(DLLType dllType, const std::string& functionName, InputParamTypes&... inputParams)
+	{
+		std::shared_lock lock(dllHandlesMutex);
+
+		auto itor = dllHandles.find(dllType);
+		if (itor == dllHandles.end())
+		{
+			return;
+		}
+
+		itor->second.CallFunction<void, InputParamTypes&...>(functionName, inputParams...);
+	}
 
 private:
 	void RunDLLLoaderThread();
--- NonstopPatcher/NonstopPatcher/main.cpp
+++ NonstopPatcher/NonstopPatcher/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include "DLLLoader.h"
 
 int main() 
 {
+	DLLManager::GetInst().StartThread();
+
+	if (DLLManager::GetInst().LoadDLL(DLLType::Func_1, "../../DLL/TestDll_1.dll") == false||
+		DLLManager::GetInst().LoadDLL(DLLType::Func_2, "../../DLL/TestDll_2.dll") == false)
+	{
+		std::cout << "LoadDLL() failed" << std::endl;
+		return 0;
+	}
+
+	const std::string funcName = "ExecuteSomethingWrapper";
+	if (DLLManager::GetInst().AddFunction<void, int&, std::string&, float&>(DLLType::Func_1, funcName) == false ||
+		DLLManager::GetInst().AddFunction<std::string, char&>(DLLType::Func_2, funcName) == false)
+	{
+		std::cout << "AddFunction() failed" << std::endl;
+		return 0;
+	}
+
+	int i = 1;
+	std::string s = "param test";
+	float f = 0.7f;
+	DLLManager::GetInst().CallFunction<void, int&, std::string&, float&>(DLLType::Func_1, funcName, i, s, f);
+
+	char c = 't';
+	auto returnOpt = DLLManager::GetInst().CallFunction<std::string, char&>(DLLType::Func_2, funcName, c);
+	if (not returnOpt.has_value())
+	{
+		std::cout << "Return is not has value" << std::endl << std::endl;
+		return 0;
+	}
+	std::cout << "After call with " << c << " / " << *returnOpt << std::endl;
+
+	DLLManager::GetInst().StopThread();
+
+	std::cout << std::endl;
 	return 0;
 }
